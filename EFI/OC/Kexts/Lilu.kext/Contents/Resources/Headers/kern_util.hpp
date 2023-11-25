a6d1374ae3787e70c755442033238893ff61ce9a
zhuweideng <1228423813@qq.com>
2023-11-25 13:55:22 +0800

    AppleALC 1.8.7 Lilu 1.6.7 NVMeFix 1.1.1 VirtualSMC 1.3.2 WhateverGreen 1.6.6

--- EFI/OC/Kexts/Lilu.kext/Contents/Resources/Headers/kern_util.hpp
+++ EFI/OC/Kexts/Lilu.kext/Contents/Resources/Headers/kern_util.hpp
@@ -263,7 +263,7 @@ extern proc_t kernproc;
  *
  *  @param format  formatted string
  */
-EXPORT extern "C" void lilu_os_log(const char *format, ...);
+EXPORT extern "C" void lilu_os_log(const char *format, ...) __printflike(1, 2);
 
 /**
  *  Two-way substring search
@@ -393,6 +393,8 @@ enum KernelVersion {
 	Catalina      = 19,
 	BigSur        = 20,
 	Monterey      = 21,
+	Ventura       = 22,
+	Sonoma        = 23,
 };
 
 /**
